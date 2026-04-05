e960abce3a9cc48ac16341208973b44740d90fc1
rohit_chandra <chandrarohit91@gmail.com>
2026-04-05 11:25:01 +0000

    only dsa

--- app/api/dependencies.py
+++
@@ -1,7 +0,0 @@
-from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.db.session import SessionLocal
-
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with SessionLocal() as session:
-        yield session
